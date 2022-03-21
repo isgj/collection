fb6847b353e5eee0138ad6d5f9da3ca79e9b86dd
Ismail Gjevori <isgjevori@protonmail.com>
2022-03-21 17:29:30 +0100

    Add Count and some docs

--- iterator.go
+++ iterator.go
@@ -1,7 +1,14 @@
 package collection
 
+// Iterator is a lazy iterator over generic data types.
+// It can be called several times to produce values.
+// When the second returned value is `true` means the value is valid and it can be consumed.
+// When the second returned value is `false` means the value is not valid.
+// In this case the zero value of the type `T` is returned and it should not be consumed.
+// Consecutive calls after the first time `false` is returned, should return the same values.
 type Iterator[T any] func() (T, bool)
 
+// Any returns true as soon as a value satisfies the test, false otherwise.
 func (it Iterator[T]) Any(test func(item T) bool) bool {
 	for i, ok := it(); ok; i, ok = it() {
 		if test(i) {
@@ -11,6 +18,7 @@ func (it Iterator[T]) Any(test func(item T) bool) bool {
 	return false
 }
 
+// Collect will consume the iterator and return a `Vec` with all the values.
 func (it Iterator[T]) Collect() Vec[T] {
 	var vec Vec[T]
 	for i, ok := it(); ok; i, ok = it() {
@@ -19,6 +27,16 @@ func (it Iterator[T]) Collect() Vec[T] {
 	return vec
 }
 
+// Count will consume the iterator and return the number of values iterated.
+func (it Iterator[T]) Count() int {
+	var c int
+	for _, ok := it(); ok; _, ok = it() {
+		c++
+	}
+	return c
+}
+
+// Every will return false as soon as a value will fail the test, true otherwise.
 func (it Iterator[T]) Every(test func(item T) bool) bool {
 	for i, ok := it(); ok; i, ok = it() {
 		if !test(i) {
@@ -28,6 +46,7 @@ func (it Iterator[T]) Every(test func(item T) bool) bool {
 	return true
 }
 
+// Filter will pass only the values that satisfy the test.
 func (it Iterator[T]) Filter(test func(item T) bool) Iterator[T] {
 	return func() (T, bool) {
 		for i, ok := it(); ok; i, ok = it() {
@@ -39,6 +58,8 @@ func (it Iterator[T]) Filter(test func(item T) bool) Iterator[T] {
 	}
 }
 
+// Find will try to find a value that satisfies the test.
+// The second returned value is true if a value was found, false otherwise.
 func (it Iterator[T]) Find(test func(item T) bool) (T, bool) {
 	for i, ok := it(); ok; i, ok = it() {
 		if test(i) {
@@ -48,6 +69,7 @@ func (it Iterator[T]) Find(test func(item T) bool) (T, bool) {
 	return *new(T), false
 }
 
+// FollowedBy will yield first the values of `it` followed by the values of `other`.
 func (it Iterator[T]) FollowedBy(other Iterator[T]) Iterator[T] {
 	other_turn := false
 	return func() (T, bool) {
@@ -62,12 +84,14 @@ func (it Iterator[T]) FollowedBy(other Iterator[T]) Iterator[T] {
 	}
 }
 
+// ForEach will consume the iterator and run the action with every value.
 func (it Iterator[T]) ForEach(action func(item T)) {
 	for i, ok := it(); ok; i, ok = it() {
 		action(i)
 	}
 }
 
+// Skip will skip the first `count` values
 func (it Iterator[T]) Skip(count int) Iterator[T] {
 	skipped := false
 	return func() (T, bool) {
@@ -85,6 +109,7 @@ func (it Iterator[T]) Skip(count int) Iterator[T] {
 	}
 }
 
+// Take will yield at most the first `count` values.
 func (it Iterator[T]) Take(count int) Iterator[T] {
 	taken := 0
 	return func() (T, bool) {
@@ -100,6 +125,8 @@ func (it Iterator[T]) Take(count int) Iterator[T] {
 	}
 }
 
+// Reverse will consume the iterator, collect the values in a `Vec` and iterate in reverse those values.
+// Since `Reverse` will consume the iterator and allocate a `Vec`, when possible use `Vec.ReverseIter`.
 func (it Iterator[T]) Reverse() Iterator[T] {
 	return it.Collect().ReverseIter()
 }
