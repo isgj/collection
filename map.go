070f6160790396136ee9245694990ea35b11778e
Ismail Gjevori <isgjevori@protonmail.com>
2022-03-24 19:05:38 +0100

    Add a few methods to map

--- map.go
+++ map.go
@@ -1,7 +1,22 @@
 package collection
 
+// Map is the same as `map` but with some methods.
 type Map[K comparable, V any] map[K]V
 
+// Clear will delete all the key/value pairs in the map.
+func (m Map[K, V]) Clear() {
+	for k := range m {
+		delete(m, k)
+	}
+}
+
+// Has checks if the key is in the map.
+func (m Map[K, V]) Has(key K) bool {
+	_, ok := m[key]
+	return ok
+}
+
+// Keys will return a Vec with the keys of the map.
 func (m Map[K, V]) Keys() Vec[K] {
 	v := make(Vec[K], 0, len(m))
 	for k := range m {
@@ -10,6 +25,17 @@ func (m Map[K, V]) Keys() Vec[K] {
 	return v
 }
 
+// Len is an alias to the `len` function.
+func (m Map[K, V]) Len() int {
+	return len(m)
+}
+
+// IsEmpty checks if the map has no entries
+func (m Map[K, V]) IsEmpty() bool {
+	return len(m) == 0
+}
+
+// Values will return a Vec with the values of the map.
 func (m Map[K, V]) Values() Vec[V] {
 	v := make(Vec[V], 0, len(m))
 	for _, val := range m {
